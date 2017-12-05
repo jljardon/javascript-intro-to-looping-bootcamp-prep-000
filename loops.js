c33a6ba0beb30b1c929b4d1dce3d49dc749bab18
Jose Luis Jardon <jljardon.c@gmail.com>
2017-12-05 00:50:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,12 @@
 function forLoop(array){
-  for (var = 1 ; i < 25; i++){
+  for (var i = 1 ; i < 25; i++){
     if(i === 1){
       array.push("I am 1 strange loop.")
     }
   }
   return array
 }
+
 
 function whileLoop(n){
 
